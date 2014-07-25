10d8a9ca1504bcf48d42214d66d82a09e834cc4d
Shuhei Hiya <syuhei176@gmail.com>
2014-07-25 15:26:05 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+(function(){
+
+var logger = {
+	separator : "\t",
+	create_prefix : function() {
+		return [];
+	},
+	stdout : function(stdout, _args) {
+		stdout.write(_args.join(this.separator) + "\n");
+	},
+	base : function(stdout, _arguments) {
+		var _args = this.create_prefix().concat();
+		for(var i=0;i < _arguments.length;i++) {
+			_args.push(_arguments[i]);
+		}
+		this.stdout(stdout, _args);
+	},
+	info : function() {
+		this.base(process.stdout, arguments);
+	},
+	error : function() {
+		this.base(process.stderr, arguments);
+	},
+	prefix : function(create_prefix) {
+		this.create_prefix = create_prefix;
+	}
+}
+
+if ('undefined' == typeof module) {
+	window.logger = logger
+} else {
+	module.exports = logger
+}
+
+}());
---
+++ test.js
@@ -0,0 +1,13 @@
+var logger = require("./index");
+
+logger.separator = "\t";
+logger.prefix(function() {
+	var date = new Date();
+	var index = (date.getYear() + 1900) + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+	return ["production", index, "app", new Date().getTime()];
+});
+
+logger.info("info", "signup", "message");
+logger.info("info", "signup", "message");
+logger.error("error", "createapp", "message");
+logger.info("info", "signup", "message");
